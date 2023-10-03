39ca1f77c4fa802f0332888e5cbc6680cdfb18df
Sebastian Mesa <sebas.mesa.montoya@gmail.com>
2023-10-03 17:52:31 -0500

    style(main): delete comments

--- main.py
+++ main.py
@@ -23,22 +23,3 @@ login(
     password=contrasena,
     lista=lista,
 )
-
-
-# with open("Empleados.txt", "r") as f:
-#     registro = List()
-#     emps = [line[:-1].split(' ') for line in f.readlines()]
-#     for emp in emps:
-#         registro.add_last(emp)
-
-# print(f"Verificación registro:\n{registro.get_first().get_data()}\n\n")
-
-# with open("Password.txt", "r") as p:
-#     ids = List()
-#     for emp in [line[:-1].split(' ') for line in p.readlines()]:
-#         ids.add_last(emp)
-# print(f"Verificación ID's:\n{ids.get_first().get_data()}\n\n")
-
-# # Login
-# print("Bienvenido al Sistema de Mensajería")
-# print("Por favor ingresa tu No. de Documento:")
